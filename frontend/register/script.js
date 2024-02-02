057ae56d99b58def4b4f129ad08e14020405b4f3
João Pedro <joaopdesousa12@gmail.com>
2024-02-02 15:41:24 -0300

    new fuctions in frontend

--- frontend/register/script.js
+++ frontend/register/script.js
@@ -1,39 +1,44 @@
 const btn = document.querySelector(".btn");
 const inputs = document.querySelectorAll("input");
 
-const array = [];
+const users = [];
 
 function redirect(url) {
   window.location.href = url;
 }
 
 function isUnique(username) {
-  if (array.includes(username)) {
+  console.log("oi");
+  if (users.includes(username)) {
     inputs[0].classList.add("usernameError");
+
     inputs[0].setAttribute(
       "placeholder",
       "Username exists, please, replace Username and try again"
     );
-    return false;
+    return;
   }
+  return true;
+}
+
+function newUser(username, password) {
+  return {
+    username: username,
+    password: password,
+  };
 }
 
 function handleBtnClick(event) {
   event.preventDefault();
 
-  const objeto = {
-    username: null,
-    password: null,
-  };
-
   const username = inputs[0].value;
   const password = inputs[1].value;
 
   if (isUnique(username)) {
-    objeto.username = username;
-    objeto.password = password;
-    array.array.push(objeto);
+    const user = newUser(username, password);
+    users.push(user);
   }
+  console.log(users);
 }
 
 btn.addEventListener("click", handleBtnClick);
